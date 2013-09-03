dd89f270906a1372fde43498d503677a7db32dcf
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-03 04:46:00 -0300

    create template class used to merge template

--- quik.py
+++ quik.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re, operator, os
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from io import StringIO
+    xrange = range
+
+
+class Template:
+    def __init__(self, content):
+        self.content = content
+        self.root_element = None
+
+    def merge(self, namespace, loader=None):
+        output = StoppableStream()
+        self.merge_to(namespace, output, loader)
+        return output.getvalue()
+
+    def ensure_compiled(self):
+        if not self.root_element:
+            self.root_element = TemplateBody(self.content)
+
+    def merge_to(self, namespace, fileobj, loader=None):
+        if loader is None: loader = NullLoader()
+        self.ensure_compiled()
+        self.root_element.evaluate(fileobj, namespace, loader)
+
