4a5b87a3bb5649383f9013efd13a83045b7fde23
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-03 02:18:02 -0300

    Add quik setup file register in pypi

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(name="quik",
+      version="0.0.1",
+      description="A fast and lightweight Python template engine",
+      author="Thiago Avelino",
+      author_email="thiago@avelino.xxx",
+      url="https://github.com/avelino/quik",
+      license="MIT",
+      zip_safe=False,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Text Processing :: Markup :: HTML'],
+      keywords="template, engine, web, fast, lightweight",
+      include_package_data=True,)
